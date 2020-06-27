7f704d648794300d1e34d5602a6c00ec9687e289
Anthony <anthonymarques@outlook.com>
2020-06-27 17:33:31 +0200

    Implementing firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Firebase modules
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'ABook'), // Required for everything
+    AngularFirestoreModule, // Only required for database features
+    AngularFireAuthModule, // Only required for auth features,
+    AngularFireStorageModule // Only required for storage features
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,19 @@
 
 export const environment = {
   production: false
+  firebaseConfig = {
+    apiKey: "AIzaSyDwZ0vNZ9U4LSzUxOKEVRja36tVSj2ruN0",
+    authDomain: "abook-9199a.firebaseapp.com",
+    databaseURL: "https://abook-9199a.firebaseio.com",
+    projectId: "abook-9199a",
+    storageBucket: "abook-9199a.appspot.com",
+    messagingSenderId: "62091966073",
+    appId: "1:62091966073:web:733a57374e6a46c05606a8",
+    measurementId: "G-L3QNR5HJ0R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 };
 
 /*
